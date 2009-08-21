11917f58e689d9d995c37a1f740da9b64efe5488
jamoozy <jamoozy@1ae644f0-869f-11de-bd5f-d5dc12eeefd8>
2009-08-21 01:23:15 +0000

    Fixed svn:ignore once and for all (I hope ... stupid eclipse). Major changes to BoxRecognizer (as promised).

--- java/src/com/fropsoft/sketch/BoxRecognizer.java
+++ java/src/com/fropsoft/sketch/BoxRecognizer.java
@@ -19,12 +19,11 @@
 
 package com.fropsoft.sketch;
 
+import java.util.Vector;
+
 import com.fropsoft.geometry.Line;
 import com.fropsoft.geometry.Shape;
 import com.fropsoft.obrik.Box;
-
-import java.util.LinkedList;
-import java.util.Vector;
 
 /**
  * Recognizes a box.
@@ -45,31 +44,34 @@ public class BoxRecognizer extends AbstractItemRecognizer
    *          The second line.
    * @return The probability these lines connect and where they connect.
    */
-  private static LineConnection gaugeConnection(Line l1, Line l2)
+  private static LGNode gaugeConnection(Line l1, Line l2)
   {
-    int l1point = LineConnection.TAIL;
-    int l2point = LineConnection.TAIL;
+    LineEnd.End l1point = LineEnd.End.TAIL;
+    LineEnd.End l2point = LineEnd.End.TAIL;
     double next, min = Double.MAX_VALUE;
 
     if ((next = l1.getStartPoint().distanceTo(l2.getStartPoint())) < min)
+      min = next;
+
+    if ((next = l1.getEndPoint().distanceTo(l2.getStartPoint())) < min)
     {
       min = next;
+      l1point = LineEnd.End.HEAD;
+      l2point = LineEnd.End.TAIL;
     }
-    else if ((next = l1.getEndPoint().distanceTo(l2.getStartPoint())) < min)
+
+    if ((next = l1.getStartPoint().distanceTo(l2.getEndPoint())) < min)
     {
       min = next;
-      l1point = LineConnection.HEAD;
+      l1point = LineEnd.End.TAIL;
+      l2point = LineEnd.End.HEAD;
     }
-    else if ((next = l1.getStartPoint().distanceTo(l2.getEndPoint())) < min)
+
+    if ((next = l1.getEndPoint().distanceTo(l2.getEndPoint())) < min)
     {
       min = next;
-      l2point = LineConnection.HEAD;
-    }
-    else if ((next = l1.getEndPoint().distanceTo(l2.getEndPoint())) < min)
-    {
-      min = next;
-      l1point = LineConnection.HEAD;
-      l2point = LineConnection.HEAD;
+      l1point = LineEnd.End.HEAD;
+      l2point = LineEnd.End.HEAD;
     }
 
     double overlap1 = l1.getBounds().overlap(l2.getBounds());
@@ -82,211 +84,21 @@ public class BoxRecognizer extends AbstractItemRecognizer
     // If this is negative, that means that one of the lines is shorter than
     // the gap between the closest endpoints.  "-1" is not a valid probability
     // (it's not in [0,1]), so return 0 instead.
-    return new LineConnection(l1, l1point, l2, l2point, gage < 0 ? 0 : gage);
+    return new LGNode(gage < 0 ? 0 : gage,
+            new LineEnd(l1, l1point), new LineEnd(l2, l2point));
   }
-
-//  /**
-//   * Represents a loop in the graph.
-//   */
-//  private static class GraphLoop
-//  {
-//    /**
-//     * Class used to store search queues for the breadth-first search.
-//     */
-//    private static class SearchQueue
-//    {
-//      /**
-//       * Sum of the probabilities of all the nodes this represents.
-//       */
-//      public double prob;
-//
-//      /**
-//       * The place this loop started.
-//       */
-//      public SearchNode root;
-//
-//      /**
-//       * The end of this loop.
-//       */
-//      public SearchNode curr;
-//
-//      /**
-//       * Creates a new search queue whose root node is representative of the
-//       * passed line.
-//       */
-//      public SearchQueue(LineNode ln)
-//      {
-//        root = curr = new SearchNode(ln, this);
-//      }
-//
-//      /**
-//       * Adds a new line to the list
-//       */
-//      public void addNewNode(LineNode ln, int prevHT, int currHT)
-//      {
-//        SearchNode next = new SearchNode(ln, this);
-//        if (prevHT == LineConnection.HEAD)
-//          curr.head = next;
-//        else
-//          curr.tail = next;
-//        if (currHT == LineConnection.HEAD)
-//          next.head = curr;
-//        else
-//          next.tail = curr;
-//        curr = next;
-//      }
-//
-//      /**
-//       * Makes a deep copy of this search queue.
-//       * @return The new search queue.
-//       */
-//      public SearchQueue clone()
-//      {
-//        SearchQueue that = new SearchQueue(root.lineNode);
-//        that.prob = this.prob;
-//
-//        int headOrTail = (root.head != null) ?
-//                LineConnection.HEAD : LineConnection.TAIL;
-//        SearchNode thisCurr = root;
-//
-//        while (thisCurr != null)
-//        {
-//          SearchNode next, thisNext;
-//          if (headOrTail == LineConnection.HEAD)
-//          {
-//            next = new SearchNode(thisCurr.head.lineNode, that);
-//            that.curr.head = next;
-//            thisNext = thisCurr.head;
-//          }
-//          else
-//          {
-//            next = new SearchNode(thisCurr.tail.lineNode, that);
-//            that.curr.tail = next;
-//            thisNext = thisCurr.tail;
-//          }
-//
-//          if (thisNext.head == thisCurr)
-//            next.head = that.curr;
-//          else
-//            next.tail = that.curr;
-//          that.curr = next;
-//          thisCurr = thisNext;
-//        }
-//        return that;
-//      }
-//
-//      public boolean equals(Object that)
-//      {
-//        // TODO
-//        return false; //that.getClass() == SearchQueue.class && ((SearchQueue)that);
-//      }
-//
-//      /**
-//       * One node in the search queue.
-//       */
-//      private static class SearchNode
-//      {
-//        /**
-//         * The line that that the head of the line this node represents is
-//         * connecting to.
-//         */
-//        public SearchNode head;
-//
-//        /**
-//         * The line that that the tail of the line this node represents is
-//         * connecting to.
-//         */
-//        public SearchNode tail;
-//
-//        /**
-//         * The queue this is a part of.
-//         */
-//        public SearchQueue queue;
-//
-//        /**
-//         * The line this node represents.
-//         */
-//        public final LineNode lineNode;
-//
-//        /**
-//         * Creates a new search node representing the passed line.
-//         */
-//        public SearchNode(LineNode lineNode, SearchQueue queue)
-//        {
-//          this.lineNode = lineNode;
-//          this.queue = queue;
-//        }
-//      }
-//    }
-//
-//    /**
-//     * Finds the best loop in a graph using breadth-first search.  This
-//     * ensures there is no 0-valued edge in the loop.  If no such loop could
-//     * be found, this will return null.
-//     *
-//     * @param graph
-//     *          The graph to search.
-//     * @return The best loop or null if none could be found.
-//     */
-//    public static GraphLoop findBestLoop(LineGraph graph)
-//    {
-//      LinkedList<SearchQueue> list = new LinkedList<SearchQueue>();
-//
-//      // Populate a list to search.
-//      for (int i = 0; i < graph.lns.length; i++)
-//        list.addLast(new SearchQueue(graph.lns[i].line));
-//
-//      // Build the list of loops.
-//      while (list.size() > 0)
-//      {
-//        SearchQueue next = list.removeFirst();
-//        if (next.head != null)
-//        {
-//          if (next.head != next.queue.root)
-//          {
-//            for (int i = 0; i < next.lineNode.head
-//              ;
-//          }
-//          else
-//          {
-//          }
-//        }
-//      }
-//
-//      return null;
-//    }
-//
-//    /**
-//     * Probability of this loop being awesome.
-//     */
-//    public final double prob;
-//
-//    /**
-//     * The lines involved in the loop.
-//     */
-//    public LinkedList<LineNode> loop;
-//
-//    /**
-//     */
-//    public GraphLoop(LineNode start)
-//    {
-//      prob = 0;
-//      loop = new LinkedList<LineNode>();
-//      loop.addLast(start);
-//    }
-//  }
 
   /**
    * A complete, undirected graph representing all lines and how they relate
-   * to each other in terms of a {@link LineConnection} objects.  Nodes are
-   * {@link LineNode} objects, and edges are {@link LineConnection} objects.
+   * to each other in terms of a {@link LGNode} objects.  Nodes are
+   * {@link LGEdge} objects, and edges are {@link LGNode} objects.
    */
   private static class LineGraph
   {
     /**
      * The line nodes.
      */
-    private LineNode[] lns;
+    private final LGEdge[] lns;
 
     /**
      * Creates a new line graph for the set of lines.
@@ -294,43 +106,44 @@ public class BoxRecognizer extends AbstractItemRecognizer
      * @param lines
      *          The set of lines to create the graph for.
      */
-    public LineGraph(Line... lines)
+    public LineGraph(int numNodes)
     {
-      lns = new LineNode[lines.length];
-      for (int i = 0; i < lines.length; i++)
-      {
-        lns[i] = new LineNode(lines[i], lines.length);
-        lns[i].tailProbs[i] = 0.0;
-        lns[i].headProbs[i] = 0.0;
-      }
+      lns = new LGEdge[numNodes];
+//      lns = new LGNode[lines.length];
+//      for (int i = 0; i < lines.length; i++)
+//      {
+//        lns[i] = new LGNode(lines[i], lines.length);
+//        lns[i].tailProbs[i] = 0.0;
+//        lns[i].headProbs[i] = 0.0;
+//      }
     }
 
-    /**
-     * Sets the connection between node i and j.
-     *
-     * @param i
-     *          The first node index.
-     * @param j
-     *          The second node index.
-     * @param lc
-     *          The relationship between the two nodes.
-     */
-    public void setLC(int i, int j, LineConnection lc)
-    {
-      if (lc.l1 != lns[i].line || lc.l2 != lns[j].line)
-        throw new RuntimeException("Wrong order.");
-
-      lns[i].tailProbs[j] = lc.side1 == LineConnection.TAIL ? lc.prob : 0.0;
-      lns[i].headProbs[j] = lc.side1 == LineConnection.HEAD ? lc.prob : 0.0;
-      lns[j].tailProbs[i] = lc.side2 == LineConnection.TAIL ? lc.prob : 0.0;
-      lns[j].headProbs[i] = lc.side2 == LineConnection.HEAD ? lc.prob : 0.0;
-    }
+//    /**
+//     * Sets the connection between node i and j.
+//     *
+//     * @param i
+//     *          The first node index.
+//     * @param j
+//     *          The second node index.
+//     * @param lc
+//     *          The relationship between the two nodes.
+//     */
+//    public void setLC(int i, int j, LGEdge lc)
+//    {
+//      if (lc.l1 != lns[i].line || lc.l2 != lns[j].line)
+//        throw new RuntimeException("Wrong order.");
+//
+//      lns[i].tailProbs[j] = lc.side1 == LineEnd.End.TAIL ? lc.prob : 0.0;
+//      lns[i].headProbs[j] = lc.side1 == LineEnd.End.HEAD ? lc.prob : 0.0;
+//      lns[j].tailProbs[i] = lc.side2 == LineEnd.End.TAIL ? lc.prob : 0.0;
+//      lns[j].headProbs[i] = lc.side2 == LineEnd.End.HEAD ? lc.prob : 0.0;
+//    }
   }
 
   /**
    * A node in the {@link LineGraph}.
    */
-  private static class LineNode
+  private static class LGEdge
   {
     /**
      * The line this is a node for.
@@ -340,12 +153,12 @@ public class BoxRecognizer extends AbstractItemRecognizer
     /**
      * Probabilities of this head connecting somewhere else.
      */
-    public final double[] headProbs;
+    public final LGNode head;
 
     /**
      * Probabilities of this tail connecting somewhere else.
      */
-    public final double[] tailProbs;
+    public final LGNode tail;
 
     /**
      * Creates a new node that is part of a graph with numLines members, for
@@ -356,11 +169,11 @@ public class BoxRecognizer extends AbstractItemRecognizer
      * @param numLines
      *          The number of nodes in the graph.
      */
-    public LineNode(Line line, int numLines)
+    public LGEdge(Line line, LGNode head, LGNode tail)
     {
       this.line = line;
-      this.headProbs = new double[numLines];
-      this.tailProbs = new double[numLines];
+      this.head = head;
+      this.tail = tail;
     }
   }
 
@@ -369,41 +182,12 @@ public class BoxRecognizer extends AbstractItemRecognizer
    * method.  This represents the probability that two lines were meant to be
    * connected AND which ends of the lines were meant to be connected.
    */
-  private static class LineConnection
+  private static class LGNode
   {
     /**
-     * Field that signifies the start point.
+     * The list of line ends that converge here.
      */
-    public static int TAIL = 1;
-
-    /**
-     * Field that signifies the end point.
-     */
-    public static int HEAD = 2;
-
-    /**
-     * The one line.
-     */
-    public final Line l1;
-
-    /**
-     * The side of the one line that's closest to the other line.
-     *
-     * @see {@link #TAIL}, {@link #HEAD}
-     */
-    public final int side1;
-
-    /**
-     * The other line.
-     */
-    public final Line l2;
-
-    /**
-     * The side of the other line that's closest to the one line.
-     *
-     * @see {@link #TAIL}, {@link #HEAD}
-     */
-    public final int side2;
+    public final Vector<LineEnd> les;
 
     /**
      * Probability that these two lines were meant to be connected.
@@ -413,29 +197,78 @@ public class BoxRecognizer extends AbstractItemRecognizer
     /**
      * Creates a new object with all data.
      *
-     * @param l1
-     *          The one line.
-     * @param side1
-     *          The side of the one line, closest to the other line.
-     * @param l2
-     *          The other line.
-     * @param side2
-     *          The side of the other line, closest to the one line.
+     * @param lineEnds
+     *          The line ends that converge here.
      * @param p
      *          The probability these lines were meant to be paired.
      */
-    public LineConnection(Line l1, int side1, Line l2, int side2, double p)
+    public LGNode(double prob, LineEnd... lineEnds)
     {
-      this.l1 = l1;
-      this.side1 = side1;
-      this.l2 = l2;
-      this.side2 = side2;
-      prob = p;
+      this.prob = prob;
+      les = new Vector<LineEnd>();
+      for (LineEnd e : lineEnds)
+        les.add(e);
     }
   }
 
   /**
-   * Creaes a new box recognizer.
+   * Represents one end of a line, either the head or the tail.  This is used
+   * by the {@link LGNode} object to group ends of lines.  Ends must be known,
+   * so that it's certain that one line doesn't have two or more lines coming
+   * out of the same end.
+   */
+  private static class LineEnd
+  {
+    public enum End
+    {
+      /**
+       * Field that signifies the start point.
+       */
+      TAIL,
+
+      /**
+       * Field that signifies the end point.
+       */
+      HEAD;
+    }
+
+    /**
+     * Specifies which end of the line.
+     *
+     * @see {@link #TAIL}, {@link #HEAD}
+     */
+    public final End end;
+
+    /**
+     * The line.
+     */
+    public final Line line;
+
+    /**
+     * Creates a new line for the specified line at the specified end.
+     */
+    public LineEnd(Line line, End end)
+    {
+      this.line = line;
+      this.end = end;
+    }
+
+    /**
+     * Determines if that is the same as this.
+     *
+     * @param that
+     *          The object to compare with.
+     */
+    @Override
+    public boolean equals(Object that)
+    {
+      return that.getClass() == LineEnd.class &&
+        ((LineEnd)that).line == line && ((LineEnd)that).end == end;
+    }
+  }
+
+  /**
+   * Creates a new box recognizer.
    */
   public BoxRecognizer()
   {
@@ -452,24 +285,16 @@ public class BoxRecognizer extends AbstractItemRecognizer
   {
     Line[] lines = Line.findLines(shapes);
 
-    LineGraph graph = new LineGraph(lines);
+    Vector<LGNode> edges = new Vector<LGNode>();
+
+//    LineGraph graph = new LineGraph(lines.length);
 
     // Build a graph that represents the relationship between all lines on the
     // canvas.
     for (int i = 0; i < lines.length; i++)
       for (int j = i + 1; j < lines.length; j++)
-        graph.setLC(i, j, BoxRecognizer.gaugeConnection(lines[i], lines[j]));
+        edges.add(BoxRecognizer.gaugeConnection(lines[i], lines[j]));
 
     return 0;
-
-//    GraphLoop loop = GraphLoop.findBestLoop(graph);
-//    if (loop == null)
-//    {
-//      return 0;
-//    }
-//    else
-//    {
-//      return loop.prob;
-//    }
   }
 }
